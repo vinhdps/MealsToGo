c0226d211cd84bce489ea14dbc4b94f682e07f6a
vinhdps27 <trinhcongvinh27022002@gmail.com>
2021-04-24 01:05:17 +0700

    css restyle

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
@@ -9,25 +9,35 @@ import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: white;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const CardPicture = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[1]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const CardInfo = styled(View)`
-  padding-bottom: ${(props) => props.theme.space[3]};
-  padding-horizontal: ${(props) => props.theme.space[3]};
   flex-direction: row;
 `;
 
 const LeftSection = styled(View)`
-  flex-direction: column;
+  flex: 1;
+  align-items: flex-start;
+  padding-left: ${(props) => props.theme.space[1]};
 `;
 
 const RightSection = styled(View)`
-  flex-direction: column;
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: ${(props) => props.theme.space[1]};
+`;
+
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-vertical: ${(props) => props.theme.space[1]};
 `;
 
 const Name = styled(Text)`
@@ -38,28 +48,32 @@ const Name = styled(Text)`
 
 const Address = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
-  font-family: ${(props) => props.theme.fonts.monospace};
+  font-family: ${(props) => props.theme.fonts.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
-const Rating = styled(View)`
-  flex-direction: row;
-  padding-vertical: ${(props) => props.theme.space[1]};
+const OpeningIcon = styled(SvgXml)`
+  width: ${(props) => props.theme.sizes[4]};
+  height: ${(props) => props.theme.sizes[4]};
+  padding-right: ${(props) => props.theme.space[6]};
 `;
 
-const Opening = styled(View)``;
+const RestaurantIcon = styled(Image)`
+  width: ${(props) => props.theme.sizes[2]};
+  height: ${(props) => props.theme.sizes[2]};
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://cdn.icon-icons.com/icons2/789/PNG/512/restaurant_icon-icons.com_65336.png",
     photos = [
       "https://media-cdn.tripadvisor.com/media/photo-s/1b/67/cc/f8/chestnut-restaurant.jpg",
     ],
-    address = "123  Random Street, Big City, Vietnam",
+    address = "123 Random Street,  Big City,  Vietnam",
     isOpenNow = true,
-    rating = 3,
-    isCloseTemporarily,
+    rating = 5,
+    isCloseTemporarily = false,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.ceil(rating)));
@@ -78,9 +92,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           <Address>{address}</Address>
         </LeftSection>
         <RightSection>
-          <Opening>
-            <SvgXml xml={open} width={35} height={35} />
-          </Opening>
+          {isOpenNow && <OpeningIcon xml={open} />}
+          <RestaurantIcon source={{ uri: icon }} />
         </RightSection>
       </CardInfo>
     </RestaurantCard>
